30854506af0b5e59181d51ccdaf21037a7dde3ff
Loky <lokyto18@gmail.com>
2025-03-28 13:24:22 +1100

    FEAT | add HotelPage -> updated from App and main.js

--- src/HotelPage.vue
+++ src/HotelPage.vue
@@ -1,24 +1,51 @@
 <template>
-  <div id="app">
+  <div id="app" class="hotel-page">
+    <HotelHeader
+      :hotelCount="hotelCount"
+      :field="{
+        options: sortOptions,
+        type: 'F_SELECT',
+        label: 'Sort by',
+      }"
+    />
+    <HotelListing
+      :hotels="sortedHotels"
+    />
   </div>
 </template>
 
 <script>
+import { mapState, mapGetters } from 'vuex';
+import data from './mock/data.json';
+import HotelHeader from './components/HotelHeader';
+import HotelListing from './components/HotelListing';
 
 export default {
-  name: 'App',
+  name: 'HotelPage',
   components: {
-  }
-}
+    HotelHeader,
+    HotelListing,
+  },
+  data: function () {
+    return {
+      sortOptions: [
+        { value: 'ASC', label: 'Price low-high' },
+        { value: 'DESC', label: 'Price high-low' },
+      ],
+      hotelCount: 0,
+    };
+  },
+  mounted() {
+    this.mockData = data;
+    console.log('mock data', this.mockData.results);
+    console.log('sorted hotels', this.sortedHotels);
+    this.hotelCount = this.hotels.length;
+  },
+  computed: {
+    ...mapState(['hotels']), // Access the 'hotels' state
+    ...mapGetters(['sortedHotels']), // Access the sorted hotels getter
+  },
+  methods: {
+  },
+};
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import Vue from 'vue'
-import HotelPage from './HotelPage.vue'
-import store from './store'
+import Vue from 'vue';
+import HotelPage from './HotelPage.vue';
+import store from './store';
+import './scss/_style.scss';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.config.devtools = true;
 
 new Vue({
   store,
-  render: h => h(HotelPage)
-}).$mount('#app')
+  render: (h) => h(HotelPage),
+}).$mount('#app');
