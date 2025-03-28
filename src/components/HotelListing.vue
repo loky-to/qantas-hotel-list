07c2cf9c08e94f3e317b88542f9960df3d0f6486
Loky <lokyto18@gmail.com>
2025-03-28 13:13:21 +1100

    FEAT | add HotelListing container

---
+++ src/components/HotelListing.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="hotel-listing">
+      <HotelListingItem
+        v-for="hotel in hotels"
+        :key="hotel.id"
+        :hotel="hotel"
+      />
+      <!-- could add pagination -->
+    </div>
+  </template>
+  
+  <script>
+  import HotelListingItem from './HotelListingItem.vue';
+  export default {
+    name: 'HotelListing',
+    components: {
+        HotelListingItem,
+    },
+    props: {
+        hotels: Array,
+    },
+  };
+  </script>
+  
