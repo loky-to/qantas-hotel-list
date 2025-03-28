1c834ece4410d6d30ed39852dc4a094b6adf00e2
Loky <lokyto18@gmail.com>
2025-03-28 11:21:30 +1100

    FEAT | build global BaseHeader and feat HotelHeader

---
+++ src/components/HotelHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hotel-header">
+    <a href="https://www.qantas.com">
+      <img src="images/qantas-logo.png" alt="Logo" />
+    </a>
+    <BaseHeader :field="field">
+      <div class="hotel-header__lhs-text">
+        <span class="hotel-header__lhs-text -bold">{{ hotelCount }}</span>
+        <span class="hotel-header__lhs-text -italic"> hotels in </span>
+        <span class="hotel-header__lhs-text -bold">Sydney.</span>
+        <!-- Sydney is hard coded in, expect this to be returned by API in prod-->
+      </div>
+    </BaseHeader>
+  </div>
+</template>
+
+<script>
+import BaseHeader from '../shared-vue/BaseHeader.vue';
+export default {
+  name: 'HotelHeader',
+  components: {
+    BaseHeader,
+  },
+  props: {
+    field: Object,
+    hotelCount: Number,
+  },
+};
+</script>
---
+++ src/shared-vue/BaseHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="base-header">
+    <slot />
+    <FormFields id="base-header-select" :field="field"/>
+  </div>
+</template>
+
+<script>
+import FormFields from './FormFields';
+export default {
+  name: 'BaseHeader',
+  components: {
+    FormFields,
+  },
+  props: {
+    field: Object,
+  },
+};
+</script>
